e3a1dafe7571b6539db5df0e255b96c33f0a9bf1
Cp <yoshyparrott@gmail.com>
2015-11-09 16:31:25 +0100

    added a date check

--- script/javascript.js
+++ script/javascript.js
@@ -1,6 +1,7 @@
 console.log (5+4)
 alert("U start nu de log")
 nameAsk();
+datumAsk();
 function nameAsk() {
 	var naam = prompt("Geef de naam");
 	if (naam.length === 0){
@@ -9,5 +10,17 @@ function nameAsk() {
 	}
 	else {
 		document.write(naam);
+		document.write("<br>");
+	}
+}
+
+function datumAsk() {
+	var datum = prompt("Geef de datum (dd-mm-jjjj)");
+	if (datum.length <= 9) {
+		alert("Geef een geldige datum op");
+		datumAsk();
+	}
+	else{
+		document.write(datum);
 	}
 }
