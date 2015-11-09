2c4306b683423348ba443a99c39ef2dceee7b750
Cp <yoshyparrott@gmail.com>
2015-11-09 16:24:59 +0100

    finallly fixed it

--- script/javascript.js
+++ script/javascript.js
@@ -1,9 +1,13 @@
 console.log (5+4)
 alert("U start nu de log")
-var naam = prompt("Geef de naam");
+nameAsk();
+function nameAsk() {
+	var naam = prompt("Geef de naam");
 	if (naam.length === 0){
-		alert ("Geen naam gegeven");
+		alert("Geen naam gegeven");
+		nameAsk();
 	}
 	else {
 		document.write(naam);
 	}
+}
