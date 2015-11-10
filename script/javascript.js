7fdeb310da9227457d3b85c6b0a92e88188c5df3
Cp <yoshyparrott@gmail.com>
2015-11-10 15:32:10 +0100

    HUGE overhall with global variables meaning it will print everything out in the end

--- script/javascript.js
+++ script/javascript.js
@@ -5,7 +5,7 @@ datumAsk();
 modemAsk();
 klachtAsk();
 function nameAsk() {
-	var naam = prompt("Geef de naam");
+	naam = prompt("Geef de naam");
 	if (naam.length === 0){
 		alert("Geen naam gegeven");
 		nameAsk();
@@ -19,7 +19,7 @@ function nameAsk() {
 }
 
 function datumAsk() {
-	var datum = prompt("Geef de datum (dd-mm-jjjj)");
+	datum = prompt("Geef de datum (dd-mm-jjjj)");
 	if (datum.length <= 9) {
 		alert("Geef een geldige datum op");
 		datumAsk();
@@ -31,7 +31,7 @@ function datumAsk() {
 }
 
 function modemAsk() {
-	var modem = prompt("Wat voor type modem gebruikt de klant?");
+	modem = prompt("Wat voor type modem gebruikt de klant?");
 	if (modem.length === 0){
 		alert("Geef een geldige modem op");
 		modemAsk();
@@ -45,13 +45,15 @@ function modemAsk() {
 function klachtAsk() {
 	var klachtCheck = confirm("Heeft de klant een klacht?");
 	if (klachtCheck = true){
-		var browsen = prompt("Kunt u browsen naar www.nu.nl?");
-		if (klacht.length === 0){
-			alert("Geef een geldige klacht op");
-			klachtAsk();
+		browsen = confirm("Kunt u browsen naar www.nu.nl?");
+		if (browsen = true){
+			browskan = ("De klant kan naar www.nu.nl");
+			document.write("De klant heeft geen problemen");
+			document.write("<br>");
+			document.write("de gegevens zijn: <br>");
+			document.write(naam+datum+modem+browskan);
 		}
 		else{
-			document.write(browsen);
 			document.write("<br>");
 		}
 	}
