a0620a4f77f8f59b662757bdee6597722f5de9c0
Cp <yoshyparrott@gmail.com>
2015-11-15 21:39:02 +0100

    killed my script bcz huge ui overhaul

--- script/javascript.js
+++ script/javascript.js
@@ -2,17 +2,9 @@
 feel free to copy my code but PLEASE JUST GIVE ME CREDIT -__- Have a nice day*/
 
 alert("U start nu de log")
-loadTable();
 nameAsk();
 datumAsk();
 modemAsk();
-klachtAsk();
-
-function loadTable() {
-	belReden0 = "klacht";
-	belReden1 = "abbonement";
-	belReden2 = "informatie";
-}
 
 function nameAsk() {
 	naam = prompt("Geef de naam");
@@ -43,27 +35,6 @@ function modemAsk() {
 	}
 	else{
 		document.write("<p class = 'modem'><span class='pre-info'>Modem: </span>" + "<span id = 'modem'>" + modem + " </span>" + "<button id='naamwijzig' class='wijzig' onclick='modemWijzig()'>Wijzig</button></p>");
-	}
-}
-
-function klachtAsk() {
-	klachtCheck = prompt("Waarom belt de klant");
-	klachtCheck = klachtCheck.toLowerCase();
-	if (klachtCheck === belReden0){
-		browsen = confirm("Kan de klant browsen naar www.nu.nl?");
-		if (browsen = true){
-			browskan = ("De klant kan naar www.nu.nl");
-		}
-	}
-	else if(klachtCheck === belReden1) {
-		/*EMPTY*/	
-	}
-	else if(klachtCheck === belReden2) {
-		/*EMPTY*/	
-	}
-	else{
-		alert("Geef een geldige klacht op");
-		klachtAsk();
 	}
 }
 
